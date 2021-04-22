023981ce9ef5deb3ecf610ea821e40a744c0d393
chiptopher <chrisboyerdev@gmail.com>
2021-04-22 08:00:45 -0400

    Read colors and text from config

---
+++ example.config.lua
@@ -0,0 +1,10 @@
+require "color"
+
+Config = {
+    backgroundColor = {color(255, 0, 0)},
+    dotColor = {color(0, 255, 0)},
+    textPrimaryColor = {color(0, 0, 255)},
+    text = "Hello World"
+}
+
+return Config
